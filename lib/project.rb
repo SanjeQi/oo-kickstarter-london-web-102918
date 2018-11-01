a27759e3dba40a818e316345ee47dbd2198a3a1e
github username <github email address>
2018-11-01 16:16:20 +0000

    create Project class , initialize title and add add_backer as an instance method

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Project
+ attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+  end
+
+  
+end
